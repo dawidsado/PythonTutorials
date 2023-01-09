eb59399c0bb7341daa4f97eb5ab21e3fae1fdfac
Dawid Sadownik <dawidplay41@interia.pl>
2023-01-09 20:28:54 +0100

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,33 @@
+#Ex1
+firstRow = 1
+lastRow = 31
+currentRow = firstRow
+
+while (currentRow <= lastRow):
+    print("Row number", currentRow)
+    currentRow+=1
+
+#Ex2
+firstNumber = 1
+lastNumber = 13
+i = firstNumber
+while (i <= lastNumber):
+    print("square", i**2)
+    print("cube", i**3)
+    i+=1
+
+#Ex3
+beg = 0
+end = 16
+i = beg
+while(i <= end):
+    print(i,"square root equals",i**2)
+    i+=1
+
+#Ex4
+beg = 1
+end = 10
+i = beg
+while (i <= end):
+    print(i*'x')
+    i+=1
