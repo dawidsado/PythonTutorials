8600794873b838cf3c1f35660ad04d51bfc7b429
Dawid Sadownik <dawidplay41@interia.pl>
2023-02-07 21:06:30 +0100

    Add files via upload

---
+++ randomModule.py
@@ -0,0 +1,68 @@
+import random
+
+#Ex1
+for i in range(0,5):
+    print(random.randint(1,100))
+
+print("-------------------------------")
+
+#Ex2
+number1 = random.randint(1,100)
+counter = 1
+number2 = random.randint(1,100)
+
+print(counter, number2)
+
+while (number1 != number2):
+    counter+=1
+    number2 = random.randint(1,100)
+    print(counter, number2)
+
+print("Attempts", counter)
+
+print("-----------------------------")
+
+#Ex3
+countries = [
+    'Uruguay',
+    'Russia',
+    'Saudi Arabia',
+    'Egypt',
+    'Spain',
+    'Portugal',
+    'Iran',
+    'Morocco',
+    'France',
+    'Denmark',
+    'Peru',
+    'Australia',
+    'Croatia',
+    'Argentina',
+    'Nigeria',
+    'Iceland',
+    'Brazil',
+    'Switzerland',
+    'Serbia',
+    'Costa Rica',
+    'Sweden',
+    'Mexico',
+    'Korea Republic',
+    'Germany',
+    'Belgium',
+    'England',
+    'Tunisia',
+    'Panama',
+    'Colombia',
+    'Japan',
+    'Senegal',
+    'Poland'
+    ]
+
+randomTeams = random.shuffle(countries)
+groupNumber = 0
+
+for i in range(len(countries)):
+    if i%4==0:
+        groupNumber+=1
+        print("----Grupa %d----" % (groupNumber))
+        print(countries[i])
