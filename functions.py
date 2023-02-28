d1ae3e4ebcb1d00a3a6cad98e33393cd2b15b18a
Dawid Sadownik <dawidplay41@interia.pl>
2023-02-28 21:21:51 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -111,3 +111,80 @@ def getDaysTillEnd(year = datetime.date.today().year, month = datetime.date.toda
     print(delta)
     return
 getDaysTillEnd()
+
+#Ex6
+
+def printAnimal(animal=" "):
+    if animal == "cat":
+        printCat()
+
+    elif animal == "bear":
+        printBear()
+
+    elif animal == "bat":
+        printBat()
+
+    else:
+        print("Cannot print",animal,"Correct values for the parameter are: cat, bear, bat")
+        return False
+    return True
+
+
+    if PrintAnimal():
+        print('The parameter was correct')
+
+    else:
+        print('The parameter was INCORRECT')
+
+    if PrintAnimal('dog'):
+        print('The parameter was correct')
+    else:
+        print('The parameter was INCORRECT')
+
+    if PrintAnimal('bat'):
+        print('The parameter was correct')
+    else:
+        print('The parameter was INCORRECT')
+
+#Ex7
+def getDaysTillEnd(year = datetime.date.today().year, month = datetime.date.today().month, day = datetime.date.today().month):
+
+    given_date = datetime.date(year,month,day)
+
+    date_end_year = datetime.date(year,12,31)
+
+    delta = (date_end_year - given_date).days
+
+    return delta
+
+#Ex8
+def printAnimal(*animals):
+    for animal in animals:
+        if animal == "cat":
+            printCat()
+
+        elif animal == "bear":
+            printBear()
+
+        elif animal == "bat":
+            printBat()
+
+        else:
+            print("Cannot print",animals,"Correct values for the parameter are: cat, bear, bat")
+            return
+
+
+printAnimal("cat","doggo")
+
+#Ex9
+def getDaysTillEnd(*dates):
+
+    for given_date in dates:
+
+        date_end_year = datetime.date(given_date.year,12,31)
+
+        delta = (date_end_year - given_date).days
+
+        print(delta)
+
+getDaysTillEnd(datetime.date(2023,3,1))
